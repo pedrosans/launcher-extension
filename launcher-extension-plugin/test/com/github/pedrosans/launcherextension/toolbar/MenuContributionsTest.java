77677b63625aa22d7648f1b1ee060b35cd3b36f0
Pedro Santos <pedrosans@gmail.com>
2017-03-07 21:59:21 -0300

    making the code compatible with java 7

--- launcher-extension-plugin/test/com/github/pedrosans/launcherextension/toolbar/MenuContributionsTest.java
+++ launcher-extension-plugin/test/com/github/pedrosans/launcherextension/toolbar/MenuContributionsTest.java
@@ -45,7 +45,7 @@ import org.junit.Test;
 
 /**
  * @author Pedro Santos
- *
+ * 
  */
 public class MenuContributionsTest {
 
@@ -213,196 +213,8 @@ public class MenuContributionsTest {
 		}
 
 		@Override
-		public <T> T getAdapter(Class<T> adapter) {
-			// TODO Auto-generated method config
-			return null;
-		}
-
-		@Override
-		public boolean contentsEqual(ILaunchConfiguration configuration) {
-			// TODO Auto-generated method config
-			return false;
-		}
-
-		@Override
-		public ILaunchConfigurationWorkingCopy copy(String name) throws CoreException {
-			// TODO Auto-generated method config
-			return null;
-		}
-
-		@Override
-		public void delete() throws CoreException {
-			// TODO Auto-generated method config
-
-		}
-
-		@Override
-		public boolean exists() {
-			// TODO Auto-generated method config
-			return false;
-		}
-
-		@Override
-		public boolean getAttribute(String attributeName, boolean defaultValue) throws CoreException {
-			// TODO Auto-generated method config
-			return false;
-		}
-
-		@Override
-		public int getAttribute(String attributeName, int defaultValue) throws CoreException {
-			// TODO Auto-generated method config
-			return 0;
-		}
-
-		@Override
-		public List<String> getAttribute(String attributeName, List<String> defaultValue) throws CoreException {
-			// TODO Auto-generated method config
-			return null;
-		}
-
-		@Override
-		public Set<String> getAttribute(String attributeName, Set<String> defaultValue) throws CoreException {
-			// TODO Auto-generated method config
-			return null;
-		}
-
-		@Override
-		public Map<String, String> getAttribute(String attributeName, Map<String, String> defaultValue)
-				throws CoreException {
-			// TODO Auto-generated method config
-			return null;
-		}
-
-		@Override
-		public String getAttribute(String attributeName, String defaultValue) throws CoreException {
-			// TODO Auto-generated method config
-			return null;
-		}
-
-		@Override
-		public Map<String, Object> getAttributes() throws CoreException {
-			// TODO Auto-generated method config
-			return null;
-		}
-
-		@Override
-		public String getCategory() throws CoreException {
-			// TODO Auto-generated method config
-			return null;
-		}
-
-		@Override
-		public IFile getFile() {
-			// TODO Auto-generated method config
-			return null;
-		}
-
-		@Override
-		public IPath getLocation() {
-			// TODO Auto-generated method config
-			return null;
-		}
-
-		@Override
-		public IResource[] getMappedResources() throws CoreException {
-			// TODO Auto-generated method config
-			return null;
-		}
-
-		@Override
-		public String getMemento() throws CoreException {
-			// TODO Auto-generated method config
-			return null;
-		}
-
-		@Override
 		public String getName() {
 			return name;
-		}
-
-		@Override
-		public Set<String> getModes() throws CoreException {
-			// TODO Auto-generated method config
-			return null;
-		}
-
-		@Override
-		public ILaunchDelegate getPreferredDelegate(Set<String> modes) throws CoreException {
-			// TODO Auto-generated method config
-			return null;
-		}
-
-		@Override
-		public ILaunchConfigurationType getType() throws CoreException {
-			// TODO Auto-generated method config
-			return null;
-		}
-
-		@Override
-		public ILaunchConfigurationWorkingCopy getWorkingCopy() throws CoreException {
-			// TODO Auto-generated method config
-			return null;
-		}
-
-		@Override
-		public boolean hasAttribute(String attributeName) throws CoreException {
-			// TODO Auto-generated method config
-			return false;
-		}
-
-		@Override
-		public boolean isLocal() {
-			// TODO Auto-generated method config
-			return false;
-		}
-
-		@Override
-		public boolean isMigrationCandidate() throws CoreException {
-			// TODO Auto-generated method config
-			return false;
-		}
-
-		@Override
-		public boolean isWorkingCopy() {
-			// TODO Auto-generated method config
-			return false;
-		}
-
-		@Override
-		public ILaunch launch(String mode, IProgressMonitor monitor) throws CoreException {
-			// TODO Auto-generated method config
-			return null;
-		}
-
-		@Override
-		public ILaunch launch(String mode, IProgressMonitor monitor, boolean build) throws CoreException {
-			// TODO Auto-generated method config
-			return null;
-		}
-
-		@Override
-		public ILaunch launch(String mode, IProgressMonitor monitor, boolean build, boolean register)
-				throws CoreException {
-			// TODO Auto-generated method config
-			return null;
-		}
-
-		@Override
-		public void migrate() throws CoreException {
-			// TODO Auto-generated method config
-
-		}
-
-		@Override
-		public boolean supportsMode(String mode) throws CoreException {
-			// TODO Auto-generated method config
-			return false;
-		}
-
-		@Override
-		public boolean isReadOnly() {
-			// TODO Auto-generated method config
-			return false;
 		}
 
 		@Override
@@ -428,6 +240,192 @@ public class MenuContributionsTest {
 			} else if (!name.equals(other.name))
 				return false;
 			return true;
+		}
+
+		@Override
+		public Object getAdapter(Class arg0) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public boolean contentsEqual(ILaunchConfiguration arg0) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public ILaunchConfigurationWorkingCopy copy(String arg0) throws CoreException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public void delete() throws CoreException {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public boolean exists() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public boolean getAttribute(String arg0, boolean arg1) throws CoreException {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public int getAttribute(String arg0, int arg1) throws CoreException {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public List getAttribute(String arg0, List arg1) throws CoreException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public Set getAttribute(String arg0, Set arg1) throws CoreException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public Map getAttribute(String arg0, Map arg1) throws CoreException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public String getAttribute(String arg0, String arg1) throws CoreException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public Map getAttributes() throws CoreException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public String getCategory() throws CoreException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public IFile getFile() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public IPath getLocation() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public IResource[] getMappedResources() throws CoreException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public String getMemento() throws CoreException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public Set getModes() throws CoreException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public ILaunchDelegate getPreferredDelegate(Set arg0) throws CoreException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public ILaunchConfigurationType getType() throws CoreException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public ILaunchConfigurationWorkingCopy getWorkingCopy() throws CoreException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public boolean hasAttribute(String arg0) throws CoreException {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public boolean isLocal() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public boolean isMigrationCandidate() throws CoreException {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public boolean isReadOnly() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public boolean isWorkingCopy() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public ILaunch launch(String arg0, IProgressMonitor arg1) throws CoreException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public ILaunch launch(String arg0, IProgressMonitor arg1, boolean arg2) throws CoreException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public ILaunch launch(String arg0, IProgressMonitor arg1, boolean arg2, boolean arg3) throws CoreException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public void migrate() throws CoreException {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public boolean supportsMode(String arg0) throws CoreException {
+			// TODO Auto-generated method stub
+			return false;
 		}
 
 	}
