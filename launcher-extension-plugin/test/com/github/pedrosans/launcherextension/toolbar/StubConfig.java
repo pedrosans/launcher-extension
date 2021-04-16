7c2bc8c475f8b225b34fa8c77dd0102c5410b824
Pedro Santos <pedrosans@gmail.com>
2021-04-16 00:21:16 -0300

    New debug.core API

--- launcher-extension-plugin/test/com/github/pedrosans/launcherextension/toolbar/StubConfig.java
+++ launcher-extension-plugin/test/com/github/pedrosans/launcherextension/toolbar/StubConfig.java
@@ -1,5 +1,6 @@
 package com.github.pedrosans.launcherextension.toolbar;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -54,187 +55,194 @@ public class StubConfig implements ILaunchConfiguration {
 	}
 
 	@Override
-	public Object getAdapter(Class arg0) {
-
+	public <T> T getAdapter(Class<T> adapter) {
 		return null;
 	}
 
 	@Override
-	public boolean contentsEqual(ILaunchConfiguration arg0) {
-
+	public boolean contentsEqual(ILaunchConfiguration configuration) {
 		return false;
 	}
 
 	@Override
-	public ILaunchConfigurationWorkingCopy copy(String arg0) throws CoreException {
-
+	public ILaunchConfigurationWorkingCopy copy(String name) throws CoreException {
 		return null;
 	}
 
 	@Override
 	public void delete() throws CoreException {
+	}
 
+	@Override
+	public void delete(int flag) throws CoreException {
 	}
 
 	@Override
 	public boolean exists() {
-
 		return false;
 	}
 
 	@Override
-	public boolean getAttribute(String arg0, boolean arg1) throws CoreException {
-
+	public boolean getAttribute(String attributeName, boolean defaultValue) throws CoreException {
 		return false;
 	}
 
 	@Override
-	public int getAttribute(String arg0, int arg1) throws CoreException {
-
+	public int getAttribute(String attributeName, int defaultValue) throws CoreException {
 		return 0;
 	}
 
 	@Override
-	public List getAttribute(String arg0, List arg1) throws CoreException {
-
+	public List<String> getAttribute(String attributeName, List<String> defaultValue) throws CoreException {
 		return null;
 	}
 
 	@Override
-	public Set getAttribute(String arg0, Set arg1) throws CoreException {
-
+	public Set<String> getAttribute(String attributeName, Set<String> defaultValue) throws CoreException {
 		return null;
 	}
 
 	@Override
-	public Map getAttribute(String arg0, Map arg1) throws CoreException {
-
+	public Map<String, String> getAttribute(String attributeName, Map<String, String> defaultValue) throws CoreException {
 		return null;
 	}
 
 	@Override
-	public String getAttribute(String arg0, String arg1) throws CoreException {
-
+	public String getAttribute(String attributeName, String defaultValue) throws CoreException {
 		return null;
 	}
 
 	@Override
-	public Map getAttributes() throws CoreException {
-
+	public Map<String, Object> getAttributes() throws CoreException {
 		return null;
 	}
 
 	@Override
 	public String getCategory() throws CoreException {
-
 		return null;
 	}
 
 	@Override
 	public IFile getFile() {
-
 		return null;
 	}
 
 	@Override
 	public IPath getLocation() {
-
 		return null;
 	}
 
 	@Override
 	public IResource[] getMappedResources() throws CoreException {
-
 		return null;
 	}
 
 	@Override
 	public String getMemento() throws CoreException {
-
 		return null;
 	}
 
 	@Override
-	public Set getModes() throws CoreException {
-
+	public Set<String> getModes() throws CoreException {
 		return null;
 	}
 
 	@Override
-	public ILaunchDelegate getPreferredDelegate(Set arg0) throws CoreException {
-
+	public ILaunchDelegate getPreferredDelegate(Set<String> modes) throws CoreException {
 		return null;
 	}
 
 	@Override
 	public ILaunchConfigurationType getType() throws CoreException {
-
 		return null;
 	}
 
 	@Override
 	public ILaunchConfigurationWorkingCopy getWorkingCopy() throws CoreException {
-
 		return null;
 	}
 
 	@Override
-	public boolean hasAttribute(String arg0) throws CoreException {
-
+	public boolean hasAttribute(String attributeName) throws CoreException {
 		return false;
 	}
 
 	@Override
 	public boolean isLocal() {
-
 		return false;
 	}
 
 	@Override
 	public boolean isMigrationCandidate() throws CoreException {
-
-		return false;
-	}
-
-	@Override
-	public boolean isReadOnly() {
-
 		return false;
 	}
 
 	@Override
 	public boolean isWorkingCopy() {
-
 		return false;
 	}
 
 	@Override
-	public ILaunch launch(String arg0, IProgressMonitor arg1) throws CoreException {
-
+	public ILaunch launch(String mode, IProgressMonitor monitor) throws CoreException {
 		return null;
 	}
 
 	@Override
-	public ILaunch launch(String arg0, IProgressMonitor arg1, boolean arg2) throws CoreException {
-
+	public ILaunch launch(String mode, IProgressMonitor monitor, boolean build) throws CoreException {
 		return null;
 	}
 
 	@Override
-	public ILaunch launch(String arg0, IProgressMonitor arg1, boolean arg2, boolean arg3) throws CoreException {
-
+	public ILaunch launch(String mode, IProgressMonitor monitor, boolean build, boolean register) throws CoreException {
 		return null;
 	}
 
 	@Override
 	public void migrate() throws CoreException {
-
 	}
 
 	@Override
-	public boolean supportsMode(String arg0) throws CoreException {
-
+	public boolean supportsMode(String mode) throws CoreException {
 		return false;
+	}
+
+	@Override
+	public boolean isReadOnly() {
+		return false;
+	}
+
+	@Override
+	public ILaunchConfiguration getPrototype() throws CoreException {
+		return null;
+	}
+
+	@Override
+	public boolean isAttributeModified(String attribute) throws CoreException {
+		return false;
+	}
+
+	@Override
+	public boolean isPrototype() {
+		return false;
+	}
+
+	@Override
+	public Collection<ILaunchConfiguration> getPrototypeChildren() throws CoreException {
+		return null;
+	}
+
+	@Override
+	public int getKind() throws CoreException {
+		return 0;
+	}
+
+	@Override
+	public Set<String> getPrototypeVisibleAttributes() throws CoreException {
+		return null;
+	}
+
+	@Override
+	public void setPrototypeAttributeVisibility(String attribute, boolean visible) throws CoreException {
 	}
 
 }
